23a6444c919f40d37edfb22480ac718ff0a2561b
Demon6590 <dima.chernytin@gmail.com>
2026-04-13 21:17:43 +0300

    add IQuestionModifier

---
+++ ExamPaper.Core/Interfaces/IQuestionModifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExamPaper.Core.Interfaces;
+/// <summary>
+/// Контракт для изменения пула экзаменационных вопросов
+/// </summary>
+public interface IQuestionModifier
+{
+    /// <summary>
+    /// Добавляет новый вопрос в пул
+    /// </summary>
+    /// <param name="question">Вопрос</param>
+    /// <exception cref="ArgumentNullException">Если question == null</exception>
+    /// <exception cref="InvalidOperationException">Если ID уже существует</exception>
+    void AddQuestion(IQuestion question);
+    /// <summary>
+    /// Удаляет вопрос по уникальному идентификатору
+    /// </summary>
+    /// <param name="questionId">GUID вопроса</param>
+    void RemoveQuestion(Guid questionId);
+    /// <summary>
+    /// Сохраняет все изменения в постоянное хранилище
+    /// </summary>
+    void SaveChanges();
+}
