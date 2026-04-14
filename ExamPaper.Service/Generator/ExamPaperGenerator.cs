bb995099619af56c38ca0df5bb2fe8e5b6315bb6
xRISOVARKAx <yle.do73777@gmail.com>
2026-04-14 11:22:44 +0300

    Update ExamPaperGenerator.cs

--- ExamPaper.Service/Generator/ExamPaperGenerator.cs
+++ ExamPaper.Service/Generator/ExamPaperGenerator.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using ExamPaper.Core.Generation;
 using ExamPaper.Core.Interfaces;
-
 namespace ExamPaper.Service.Generator;
-
 public class ExamPaperGenerator : IExamGenerator
 {
     public IEnumerable<IExamPaper> Generate(IEnumerable<IQuestion> availableQuestions, IGenerationSettings settings)
     {
         var questions = availableQuestions.ToList();
         var random = new Random();
-        
         return Enumerable.Range(1, settings.TotalTicketsCount)
             .Select(ticketNum => new ExamPaper(
                 ticketNum, 
