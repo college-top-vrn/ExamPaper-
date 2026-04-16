e51342652607dfa88829309bc3d4be588e711f78
tapok228227 <vseobsaitoria690@gmail.com>
2026-04-16 10:19:58 +0300

    исправил

--- ExamPaper.Core/Model/QuestionModifiler.cs
+++ ExamPaper.Core/Model/QuestionModifiler.cs
@@ -6,25 +6,43 @@ using ExamPaper.Core.Interfaces;
 
 namespace ExamPaper.Core.Model;
 
+/// <summary>
+/// Позволяет управлять списком вопросов: добавлять, удалять и фиксировать изменения.
+/// </summary>
 public class QuestionModifiler : IQuestionModifier
 {
+    /// <summary>
+    /// Список вопросов, с которым работает модификатор.
+    /// </summary>
     public List<IQuestion> Questions { get; } = new List<IQuestion>();
     
+    /// <summary>
+    /// Добавляет новый вопрос в список.
+    /// </summary>
+    /// <param name="question">Вопрос, который нужно добавить.</param>
     public void AddQuestion(IQuestion question)
     {
         Questions.Add(question);
     }
 
+    /// <summary>
+    /// Удаляет вопрос из списка по указанному идентификатору.
+    /// </summary>
+    /// <param name="questionId">Идентификатор вопроса, который нужно удалить.</param>
     public void RemoveQuestion(Guid questionId)
     {
-        IQuestion objToDelete = Questions.FirstOrDefault(x => x.Id == questionId);
+        IQuestion? objToDelete = Questions.FirstOrDefault(x => x.Id == questionId);
         
-        if (objToDelete != null)
+        if (objToDelete is not null)
         {
             Questions.Remove(objToDelete);
         }
     }
 
+    /// <summary>
+    /// Сохраняет изменения в вопросах (например, в базе данных или хранилище).
+    /// Пока не реализовано.
+    /// </summary>
     public void SaveChanges()
     {
         throw new NotImplementedException();
