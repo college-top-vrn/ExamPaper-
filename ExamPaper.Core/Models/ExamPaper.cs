a3eaaef0968407ccd768e26cf0215cc4981366d4
Khezer <hezer.fil@gmail.com>
2026-04-17 13:05:24 +0300

    Исправил ExamPaper

--- ExamPaper.Core/Models/ExamPaper.cs
+++ ExamPaper.Core/Models/ExamPaper.cs
@@ -27,8 +27,21 @@ public sealed class ExamPaper : IExamPaper
     /// </summary>
     public IReadOnlyCollection<IQuestion> Questions { get; init; }
 
+
     /// <summary>
-    /// Конструктор для создания экзаменационного билета с параметрами.
+    /// Конструктор для создания билета с автоматической генерацией Id (Version 7).
+    /// </summary>
+    /// <param name="title">Название билета.</param>
+    /// <param name="questions">Список вопросов, попавших в данный билет.</param>
+    /// <exception cref="ArgumentNullException">Если параметры пусты.</exception>
+    public ExamPaper(string title, IEnumerable<IQuestion> questions)
+        : this(Guid.CreateVersion7(), title, questions)
+    {
+    }
+
+
+    /// <summary>
+    /// Конструктор для создания экзаменационного билета с заданным Id.
     /// </summary>
     /// <param name="id">Уникальный Guid идентификатор билета.</param>
     /// <param name="title">Название билета.</param>
@@ -36,7 +49,8 @@ public sealed class ExamPaper : IExamPaper
     /// <exception cref="ArgumentNullException">Если список вопросов пуст.</exception>
     public ExamPaper(Guid id, string title, IEnumerable<IQuestion> questions)
     {
-        this.Id = id;
+        Id = id == Guid.Empty ? Guid.CreateVersion7() : id;
+
         Title = title ?? throw new ArgumentNullException(nameof(title));
         Questions = new ReadOnlyCollection<IQuestion>(
             (questions ?? throw new ArgumentNullException(nameof(questions))).ToList()
