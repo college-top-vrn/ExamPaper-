c484a5349c296e4bf9d99afde70357a144f05f07
Demon6590 <dima.chernytin@gmail.com>
2026-04-19 18:01:48 +0300

    испровление ошибки в API

--- ExamPaper.Api/Program.cs
+++ ExamPaper.Api/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Reflection;
 
 using ExamPaper.Core.Interfaces;
 using ExamPaper.Infrastructure.Repositories;
@@ -9,18 +11,21 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 const string version = "v1";
-const string localHost = "http://localhost:5047";
 const string frontendExamPaperPolicy = "FrontendExamPaperPolicy";
 
 var builder = WebApplication.CreateBuilder();
 
-builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
+string projectRoot = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+string dataFolder = Path.Combine(projectRoot, "Data");
+string filePath = Path.Combine(dataFolder, "Blank.json");
+
+builder.Services.AddScoped<IQuestionRepository, QuestionRepository>((_) => new QuestionRepository(filePath));
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(frontendExamPaperPolicy, policy =>
     {
-        policy.WithOrigins(localHost)
+        policy.WithOrigins()
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
@@ -38,24 +43,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors(frontendExamPaperPolicy);
-/// <summary>
-/// GET /api/questions
-/// Получает все вопросы экзамена.
-/// </summary>
-/// <param name="repo">Репозиторий вопросов, автоматически инжектируется DI контейнером.</param>
-/// <returns>Список всех вопросов в формате JSON.</returns>
-/// <response code="200">Успешно возвращены все вопросы.</response>
-app.MapGet("/api/questions", (IQuestionRepository repo) => 
+app.MapGet("/api/questions", (IQuestionRepository repo) =>
     repo.GetAllQuestions());
-/// <summary>
-/// GET /api/questions/{id:guid}
-/// Получает вопрос по уникальному идентификатору.
-/// </summary>
-/// <param name="id">Уникальный идентификатор вопроса (GUID).</param>
-/// <param name="repo">Репозиторий вопросов, автоматически инжектируется DI контейнером.</param>
-/// <returns>Вопрос в формате JSON или 404 если не найден.</returns>
-/// <response code="200">Вопрос успешно найден и возвращен.</response>
-/// <response code="404">Вопрос с указанным ID не найден.</response>
 app.MapGet("/api/questions/{id:guid}", (Guid id, IQuestionRepository repo) =>
 {
     var question = repo.GetQuestionById(id);
