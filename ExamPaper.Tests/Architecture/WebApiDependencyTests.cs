b41011c14e2db16dcb0a7e83b614af88195875c7
The-Blight <hezer.fil@gmail.com>
2026-04-19 17:13:24 +0300

    Создал файл с тестом зависимостей WebApi

---
+++ ExamPaper.Tests/Architecture/WebApiDependencyTests.cs
@@ -0,0 +1,21 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Fluent.Syntax.Elements.Types.Classes;
+using ArchUnitNET.Fluent.Syntax.Elements.Types.Interfaces;
+using ArchUnitNET.Loader;
+using ArchUnitNET.xUnit;
+
+using ExamPaper.Core.Models;
+using ExamPaper.Infrastructure.Repositories;
+using ExamPaper.Service.Generator;
+
+using Xunit;
+
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace ExamPaper.Tests.Architecture;
+
+public class WebApiDependencyTests
+{
+    
+    
+}
