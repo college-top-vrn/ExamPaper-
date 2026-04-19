6f14b87efcb4141c8f895dfa0db42614fceea896
The-Blight <hezer.fil@gmail.com>
2026-04-19 15:32:55 +0300

    Настроил webApi

---
+++ ExamPaper.Api.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace ExamPaper.Api.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ ExamPaper.Api/Program.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+const string version = "v1";
+const string localHost = "http://localhost:5047";
+const string frontendExamPaperPolicy = "FrontendExamPaperPolicy";
+
+var builder = WebApplication.CreateBuilder();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(frontendExamPaperPolicy, policy =>
+    {
+        policy.WithOrigins(localHost)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options => { options.SwaggerEndpoint($"/openapi/{version}.json", version); });
+}
+
+
+app.UseHttpsRedirection();
+app.UseCors(frontendExamPaperPolicy);
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
