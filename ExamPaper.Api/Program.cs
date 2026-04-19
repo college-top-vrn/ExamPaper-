7c161c8adf400c0661c4f48d7704a12ec0b59788
Demon6590 <dima.chernytin@gmail.com>
2026-04-19 17:14:18 +0300

    добовление методов API

--- ExamPaper.Api/Program.cs
+++ ExamPaper.Api/Program.cs
@@ -1,4 +1,10 @@
+using System;
+
+using ExamPaper.Core.Interfaces;
+using ExamPaper.Infrastructure.Repositories;
+
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,6 +14,7 @@ const string frontendExamPaperPolicy = "FrontendExamPaperPolicy";
 
 var builder = WebApplication.CreateBuilder();
 
+builder.Services.AddScoped<IQuestionRepository, QuestionRepository>();
 
 builder.Services.AddCors(options =>
 {
@@ -29,6 +36,30 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options => { options.SwaggerEndpoint($"/openapi/{version}.json", version); });
 }
 
-
 app.UseHttpsRedirection();
 app.UseCors(frontendExamPaperPolicy);
+/// <summary>
+/// GET /api/questions
+/// Получает все вопросы экзамена.
+/// </summary>
+/// <param name="repo">Репозиторий вопросов, автоматически инжектируется DI контейнером.</param>
+/// <returns>Список всех вопросов в формате JSON.</returns>
+/// <response code="200">Успешно возвращены все вопросы.</response>
+app.MapGet("/api/questions", (IQuestionRepository repo) => 
+    repo.GetAllQuestions());
+/// <summary>
+/// GET /api/questions/{id:guid}
+/// Получает вопрос по уникальному идентификатору.
+/// </summary>
+/// <param name="id">Уникальный идентификатор вопроса (GUID).</param>
+/// <param name="repo">Репозиторий вопросов, автоматически инжектируется DI контейнером.</param>
+/// <returns>Вопрос в формате JSON или 404 если не найден.</returns>
+/// <response code="200">Вопрос успешно найден и возвращен.</response>
+/// <response code="404">Вопрос с указанным ID не найден.</response>
+app.MapGet("/api/questions/{id:guid}", (Guid id, IQuestionRepository repo) =>
+{
+    var question = repo.GetQuestionById(id);
+    return question != null ? Results.Ok(question) : Results.NotFound();
+});
+
+app.Run();
